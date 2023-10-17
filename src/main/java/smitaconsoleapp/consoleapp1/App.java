6fe28e6248eead886f32c623252038448ff1ebd0
Smita <smita@git.com>
2023-10-17 16:02:25 +0530

    first commit

---
+++ src/main/java/smitaconsoleapp/consoleapp1/App.java
@@ -0,0 +1,13 @@
+package smitaconsoleapp.consoleapp1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
